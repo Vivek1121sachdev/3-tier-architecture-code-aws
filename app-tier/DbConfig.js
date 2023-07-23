110b0b5b0539af75995d3c35b7004e69c5d99cb7
vivek1121sachdev <viveksachdev@Asus.localdomain>
2023-07-23 21:09:30 +0530

    application-code

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.ct5ulwbya51u.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Vivek.123',
+    DB_DATABASE : 'webappdb'
+});
